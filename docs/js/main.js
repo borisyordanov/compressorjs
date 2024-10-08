23e6238ac2561f42456e73942e9ad797fe40e11a
Boris <boris.yordanov2@gmail.com>
2024-10-08 15:20:34 +0300

    enabled multi file compression

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,13 +1,77 @@
-window.addEventListener('DOMContentLoaded', function () {
+function downloadBlob(blob, filename) {
+  // Create an object URL for the blob object
+  const url = window.URL.createObjectURL(blob);
+
+  // Create a new anchor element
+  const a = document.createElement("a");
+
+  // Set the href and download attributes for the anchor element
+  // You can optionally set other attributes like `title`, etc
+  // Especially, if the anchor element will be attached to the DOM
+  a.href = url;
+  a.download = filename || "download";
+
+  // Click handler that releases the object URL after the element has been clicked
+  // This is required for one-off downloads of the blob content
+  const clickHandler = () => {
+    // window.showSaveFilePicker();
+    setTimeout(() => {
+      URL.revokeObjectURL(url);
+      removeEventListener("click", clickHandler);
+    }, 150);
+  };
+
+  // Add the click event listener on the anchor element
+  // Comment out this line if you don't want a one-off download of the blob content
+  a.addEventListener("click", clickHandler, false);
+
+  // Programmatically trigger a click on the anchor element
+  // Useful if you want the download to happen automatically
+  // Without attaching the anchor element to the DOM
+  // Comment out this line if you don't want an automatic download of the blob content
+  //a.click();
+  // Return the anchor element
+  // Useful if you want a reference to the element
+  // in order to attach it to the DOM or use it in some other way
+  return a.click();
+}
+
+const stringifyOptions = (
+  options,
+  fields = [
+    "maxWidth",
+    "maxHeight",
+    "minWidth",
+    "minHeight",
+    "width",
+    "height",
+    "resize",
+    "quality",
+    "convertTypes",
+    "convertSize",
+  ]
+) => {
+  return fields.reduce((acc, field, index) => {
+    if (options[field]) {
+      if (index === fields.length - 1) {
+        return (acc += `${field}-${options[field]}`);
+      }
+      acc += `${field}-${options[field]},`;
+    }
+    return acc;
+  }, "");
+};
+
+window.addEventListener("DOMContentLoaded", function () {
   var Vue = window.Vue;
   var URL = window.URL || window.webkitURL;
   var XMLHttpRequest = window.XMLHttpRequest;
   var Compressor = window.Compressor;
 
-  Vue.component('VueCompareImage', window.vueCompareImage);
+  Vue.component("VueCompareImage", window.vueCompareImage);
 
   new Vue({
-    el: '#app',
+    el: "#app",
 
     data: function () {
       var vm = this;
@@ -23,27 +87,38 @@ window.addEventListener('DOMContentLoaded', function () {
           minHeight: 0,
           width: undefined,
           height: undefined,
-          resize: 'none',
+          resize: "none",
           quality: 0.8,
-          mimeType: '',
-          convertTypes: 'image/png',
+          mimeType: "",
+          convertTypes: "image/png",
           convertSize: 5000000,
           success: function (result) {
-            console.log('Output: ', result);
+            console.log("Output: ", result);
+            console.log("Output size: ", this.options);
+
+            const splitName = result.name.split(".");
+            const name =
+              splitName[0] +
+              "-compressed" +
+              stringifyOptions(this.options) +
+              "." +
+              splitName[1];
+
+            downloadBlob(result, name);
 
             if (URL) {
               vm.outputURL = URL.createObjectURL(result);
             }
 
             vm.output = result;
-            vm.$refs.input.value = '';
+            vm.$refs.input.value = "";
           },
           error: function (err) {
             window.alert(err.message);
           },
         },
-        inputURL: '',
-        outputURL: '',
+        inputURL: "",
+        outputURL: "",
         input: {},
         output: {},
       };
@@ -55,14 +130,14 @@ window.addEventListener('DOMContentLoaded', function () {
         var megabyte = kilobyte * kilobyte;
 
         if (size > megabyte) {
-          return (size / megabyte).toFixed(2) + ' MB';
+          return (size / megabyte).toFixed(2) + " MB";
         } else if (size > kilobyte) {
-          return (size / kilobyte).toFixed(2) + ' KB';
+          return (size / kilobyte).toFixed(2) + " KB";
         } else if (size >= 0) {
-          return size + ' B';
+          return size + " B";
         }
 
-        return 'N/A';
+        return "N/A";
       },
     },
 
@@ -72,7 +147,7 @@ window.addEventListener('DOMContentLoaded', function () {
           return;
         }
 
-        console.log('Input: ', file);
+        console.log("Input: ", file);
 
         if (URL) {
           this.inputURL = URL.createObjectURL(file);
@@ -83,14 +158,17 @@ window.addEventListener('DOMContentLoaded', function () {
       },
 
       change: function (e) {
-        this.compress(e.target.files ? e.target.files[0] : null);
+        if (!e.target.files.length) {
+          return;
+        }
+        [...e.target.files].forEach(this.compress);
       },
 
-      dragover: function(e) {
+      dragover: function (e) {
         e.preventDefault();
       },
 
-      drop: function(e) {
+      drop: function (e) {
         e.preventDefault();
         this.compress(e.dataTransfer.files ? e.dataTransfer.files[0] : null);
       },
@@ -106,25 +184,22 @@ window.addEventListener('DOMContentLoaded', function () {
     },
 
     mounted: function () {
-      if (!XMLHttpRequest) {
-        return;
-      }
-
-      var vm = this;
-      var xhr = new XMLHttpRequest();
-
-      xhr.onload = function () {
-        var blob = xhr.response;
-        var date = new Date();
-
-        blob.lastModified = date.getTime();
-        blob.lastModifiedDate = date;
-        blob.name = 'picture.jpg';
-        vm.compress(blob);
-      };
-      xhr.open('GET', 'images/picture.jpg');
-      xhr.responseType = 'blob';
-      xhr.send();
+      // if (!XMLHttpRequest) {
+      //   return;
+      // }
+      // var vm = this;
+      // var xhr = new XMLHttpRequest();
+      // xhr.onload = function () {
+      //   var blob = xhr.response;
+      //   var date = new Date();
+      //   blob.lastModified = date.getTime();
+      //   blob.lastModifiedDate = date;
+      //   blob.name = "picture.jpg";
+      //   vm.compress(blob);
+      // };
+      // xhr.open("GET", "images/picture.jpg");
+      // xhr.responseType = "blob";
+      // xhr.send();
     },
   });
 });
